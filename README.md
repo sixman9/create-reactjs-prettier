ac73f2ae8036428197ae74ef3e4cebbfd7bee416
sixman9 <sixman9@gmail.com>
2020-04-27 23:22:27 +0100

    Added TypeScript project creation.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 Prettier-Linted-CRA
 
-A simple, interactive, Bash script configuring AirBNB's ESLint and Prettier configurations during new Create-React-App-based ReactJS project creation (Javascript/JSX).  
+A simple, interactive, Bash script configuring AirBNB's ESLint and Prettier configurations during new Create-React-App-based ReactJS project creation (Javascript or TypeScript).  
 
 The script also adds linting during Git commits via Husky (Git Hooks).  
 
 1. To run the script, copy and execute the following command:  
 `bash <(curl -s https://raw.githubusercontent.com/sixman9/create-reactjs-prettier/master/createReactJSPrettier.sh)`  
 
-2. The script will ask the project name and where to create the new ReactJS Javascript project. **N.B. it *doesn't* create Typescript-based projects, yet).**  
+2. The script will ask for the project name and where to create the new ReactJS project and whether you want it in Javascript or TypeScript.
 
 In order to have Visual Studio Code (VSCode) alert you to any Prettier and ESLint formatting issues, please install the following VSCode plugins:
 
@@ -20,6 +20,7 @@ This simple script was made possible via the following projects:
 
 - Create-React-App - https://github.com/facebook/create-react-app
 - eslint-prettier-airbnb-react - https://github.com/paulolramos/eslint-prettier-airbnb-react
+- cra-template-ts-prettier-eslint-airbnb - https://github.com/lindomar-oliveira/cra-template-ts-prettier-eslint-airbnb
 - Prettier - https://prettier.io
 - ESLint - https://eslint.org/
 - AirBnB ESLint configuaration - https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb
--- createReactJSPrettier.sh
+++ createReactJSPrettier.sh
@@ -10,10 +10,13 @@ if [ -z "$projName" ]; then
     projName=$defaultName
 fi
 
+# make $projName lowercase
+projName=`echo $projName | tr '[:upper:]' '[:lower:]'`
+
 # echo $projName
 
 # Get user's chosen directory
-read -p "Enter the parent directory [$defaultDir]: " projDir 
+read -p "Enter the parent directory [$defaultDir]: " projDir
 
 if [ -z "$projDir" ]; then
     projDir=$defaultDir
@@ -26,17 +29,31 @@ if [ ! -d "$projDir" ]; then
     mkdir -p "$projDir"
 fi
 
-echo "$projDir"/"$projName"
-
 cd "$projDir"
 
-# Create-React-App - 
-npx create-react-app $projName
-cd "$projName"
+# Create-React-App - https://github.com/facebook/create-react-app
 
-# Add AirBNB-derived prettier and linting config - https://github.com/paulolramos/eslint-prettier-airbnb-react
-#See also https://blog.echobind.com/integrating-prettier-eslint-airbnb-style-guide-in-vscode-47f07b5d7d6a for instructions
-exec 3<&1;bash <&3 <(curl https://raw.githubusercontent.com/paulolramos/eslint-prettier-airbnb-react/master/eslint-prettier-config.sh 2> /dev/null)
+PS3='Which programming language do you want for your project?: '
+options=("JavaScript" "TypeScript")
+select opt in "${options[@]}"; do
+    case "$opt" in
+    "JavaScript")
+        npx create-react-app $projName
+
+        # Add AirBNB-derived prettier and linting config - https://github.com/paulolramos/eslint-prettier-airbnb-react
+        #See also https://blog.echobind.com/integrating-prettier-eslint-airbnb-style-guide-in-vscode-47f07b5d7d6a for instructions
+        exec 3<&1 bash <&3 <(curl https://raw.githubusercontent.com/paulolramos/eslint-prettier-airbnb-react/master/eslint-prettier-config.sh 2>/dev/null)
+        break
+        ;;
+    "TypeScript")
+        npx create-react-app $projName --template ts-prettier-eslint-airbnb
+        break
+        ;;
+    *) echo Invalid ;;
+    esac
+done
+
+cd ./"$projName"
 
 # Run prettier at Git commit with 'Pretty-quick' and Husky
 # https://github.com/azz/pretty-quick
@@ -47,4 +64,6 @@ yarn add --dev pretty-quick husky
 # Add husky config to package.json - see https://stackoverflow.com/questions/25329241/edit-package-json-from-command-line for npe->package.json manipulation
 npx npe husky.hooks.pre-commit "pretty-quick --staged"
 
-yarn start
+echo -e 
+echo "Run 'cd $projDir/$projName' to enter your new project directory. 'yarn start' for a dev build."
+echo -e
